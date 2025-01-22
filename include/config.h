aba09b9d99734c69ebd164aa44f52858f9c9aceb
rdhwan <fathanridhwann@gmail.com>
2025-01-22 15:06:17 +0700

    feat: push post joga
    belum di beresin maap ya

--- include/config.h
+++ include/config.h
@@ -1,7 +1,11 @@
-#define GW_ID 100
-#define URL "http://node.mysalak.com/raspi"
-#define URL_DISTRESS "http://node.mysalak.com/node/send-notification"
+#define GW_ID 4
 
-#define CURRENT_VERSION 1000
+// prod
+#define URL "http://node.mysalak.com/raspi"
+#define URL_DISTRESS "http://node.mysalak.com/node/notify-admin"
+
+// prod
+#define CURRENT_VERSION 3010
+
 #define URL_OTA "http://ota.mysalak.com/ota"
 #define OTA_CHECK_INTERVAL 60000
